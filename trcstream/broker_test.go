9c5933ed1bbbf1c604d35e00537816d8305f6b75
Peter Bourgon <peter@bourgon.org>
2023-08-08 18:42:47 +0200

    Broker benchmark

---
+++ trcstream/broker_test.go
@@ -0,0 +1,70 @@
+package trcstream_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/peterbourgon/trc"
+	"github.com/peterbourgon/trc/trcstream"
+)
+
+func BenchmarkBroker(b *testing.B) {
+	ctx := context.Background()
+
+	b.Run("no subscribers", func(b *testing.B) {
+		broker := trcstream.NewBroker()
+
+		_, tr := trc.New(ctx, "source", "category")
+		defer tr.Finish()
+
+		for i := 0; i < b.N; i++ {
+			broker.Publish(ctx, tr)
+		}
+	})
+
+	b.Run("one skip subscriber", func(b *testing.B) {
+		var (
+			broker      = trcstream.NewBroker()
+			ctx, cancel = context.WithCancel(ctx)
+			tracec      = make(chan trc.Trace)
+		)
+
+		go func() {
+			broker.Stream(ctx, trc.Filter{IsErrored: true}, tracec)
+			close(tracec)
+		}()
+
+		_, tr := trc.New(ctx, "source", "category")
+		defer tr.Finish()
+
+		for i := 0; i < b.N; i++ {
+			broker.Publish(ctx, tr)
+		}
+
+		cancel()
+		<-tracec
+	})
+
+	b.Run("one send subscriber", func(b *testing.B) {
+		var (
+			broker      = trcstream.NewBroker()
+			ctx, cancel = context.WithCancel(ctx)
+			tracec      = make(chan trc.Trace)
+		)
+
+		go func() {
+			broker.Stream(ctx, trc.Filter{IsActive: true}, tracec)
+			close(tracec)
+		}()
+
+		_, tr := trc.New(ctx, "source", "category")
+		defer tr.Finish()
+
+		for i := 0; i < b.N; i++ {
+			broker.Publish(ctx, tr)
+		}
+
+		cancel()
+		<-tracec
+	})
+}
